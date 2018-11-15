d8b70d243b46a16c76d78bd9f5655d1346fce3ce
pankaj vats <pankajvats45@gmail.com>
2018-11-15 16:03:31 +0530

    Initial Commit

---
+++ Weather_Shortcut(API)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_Shortcut(API)
+//
+//  Created by pankaj vats on 15/11/18.
+//  Copyright © 2018 pankaj vats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
